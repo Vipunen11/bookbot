8377293d5bd6a955660ee844a722550e69d857b6
Vipunen11 <vipunen11@gmail.com>
2025-03-25 19:57:21 +0200

    Added functionality for printing out the count for individual alphanumeric characters found within the text and some print commands for pretty output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, dict_sort
 
 
 ## This function will use filepath and return contents of a txt file
@@ -7,9 +7,17 @@ def get_book_text(filepath):
 
 ## Main function is defined here
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     with open('books/frankenstein.txt', encoding="utf-8") as f:
-        print(f"{(count_words(get_book_text(f)))} words found in the document") 
+        print(f"Found {(count_words(get_book_text(f)))} total words")
+    print("--------- Character Count -------")
     with open('books/frankenstein.txt', encoding="utf-8") as text:    
-        print(count_chars(get_book_text(text)))
+        sorted_character_list = (dict_sort(count_chars(get_book_text(text))))
+        for character in sorted_character_list:
+            if character["char"].isalpha():
+                print(f"{character['char']}: {character['count']}")
+    print("============= END ===============")
     return
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,13 @@ def count_chars(text):
             char_dict[char] += 1
     return char_dict
 
+def sort_on(dictionary):
+    return dictionary["count"]
+
+## Function that takes a dictionary and returns a list of dictionaries, sorted by the value, highest first.
+def dict_sort(dictionary):
+    sorted_dict = []
+    for k,v in dictionary.items():
+        sorted_dict.append({"char": k,"count": v})
+    sorted_dict.sort(reverse=True, key=sort_on)
+    return sorted_dict
