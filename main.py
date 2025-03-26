8cd8734295f8774981b19412610ce5871bbbf5bc
Vipunen11 <vipunen11@gmail.com>
2025-03-26 11:38:37 +0200

    Refactor, remove hardcoded paths and use sys.argv[1] instead as the path to book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import count_words, count_chars, dict_sort
-
+import sys
 
 ## This function will use filepath and return contents of a txt file
 def get_book_text(filepath):
@@ -7,17 +7,19 @@ def get_book_text(filepath):
 
 ## Main function is defined here
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    with open('books/frankenstein.txt', encoding="utf-8") as f:
-        print(f"Found {(count_words(get_book_text(f)))} total words")
-    print("--------- Character Count -------")
-    with open('books/frankenstein.txt', encoding="utf-8") as text:    
-        sorted_character_list = (dict_sort(count_chars(get_book_text(text))))
+    with open(sys.argv[1], encoding="utf-8") as f:
+        book_text = get_book_text(f)
+        print(f"Found {(count_words(book_text))} total words")
+        print("--------- Character Count -------")
+        sorted_character_list = (dict_sort(count_chars(book_text)))
         for character in sorted_character_list:
             if character["char"].isalpha():
                 print(f"{character['char']}: {character['count']}")
     print("============= END ===============")
-    return
 main()
